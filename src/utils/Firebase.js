3a62f3937ac65096c0b2a372e1f72776848ff04c
abhinand.p <103823872+abhinand-psq@users.noreply.github.com>
2023-02-28 15:28:36 +0530

    Update Firebase.js

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/compat/app'
-import 'firebase/firestore'
+import 'firebase/compat/firestore'
+
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -12,4 +13,5 @@ const firebaseConfig = {
     measurementId: "G-L8QJNPVTEH"
   };
 
-  export const db=firebase.analytics(firebaseConfig)
+export const db=firebase.initializeApp(firebaseConfig)
+  
